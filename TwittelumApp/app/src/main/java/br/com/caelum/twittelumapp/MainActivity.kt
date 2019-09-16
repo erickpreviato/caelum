c444bd8f4ed0c7e5d2c681317c5be8af4976f820
Erick Previato <erick.previato@gmail.com>
2019-09-16 13:58:07 -0300

    Realizado o exercicio 2

--- TwittelumApp/app/src/main/java/br/com/caelum/twittelumapp/MainActivity.kt
+++ TwittelumApp/app/src/main/java/br/com/caelum/twittelumapp/MainActivity.kt
@@ -2,11 +2,22 @@ package br.com.caelum.twittelumapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        publicaTweet()
+    }
+
+    private fun publicaTweet() {
+        btn_publica.setOnClickListener {
+            val tweet = mensagem.text.toString()
+            Toast.makeText(this, tweet, Toast.LENGTH_LONG).show();
+        }
     }
 }
